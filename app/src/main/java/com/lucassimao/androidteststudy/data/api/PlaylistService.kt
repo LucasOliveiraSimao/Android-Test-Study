fa0ea4a59c76905be4b7755068eb2fb0e3004fc0
Lucas <oliveira.lucas20@gmail.com>
2022-09-01 23:48:36 -0400

    Refactors

--- app/src/main/java/com/lucassimao/androidteststudy/data/api/PlaylistService.kt
+++ app/src/main/java/com/lucassimao/androidteststudy/data/api/PlaylistService.kt
@@ -5,8 +5,9 @@ import com.lucassimao.androidteststudy.domain.entity.PlaylistRaw
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
-class PlaylistService(private val api: PlaylistAPI) {
+class PlaylistService @Inject constructor(private val api: PlaylistAPI) {
 
     suspend fun fetchPlaylists(): Flow<Result<List<PlaylistRaw>>> {
         return flow { emit(Result.success(api.fetchPlaylist())) }
