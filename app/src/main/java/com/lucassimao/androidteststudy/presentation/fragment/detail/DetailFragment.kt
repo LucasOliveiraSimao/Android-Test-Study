ef0b8fb2ef5ff1fd354ad6a9d0a9125499f357eb
Lucas <oliveira.lucas20@gmail.com>
2022-09-05 17:14:42 -0400

    Implements playlistDetail

--- app/src/main/java/com/lucassimao/androidteststudy/presentation/fragment/detail/DetailFragment.kt
+++ app/src/main/java/com/lucassimao/androidteststudy/presentation/fragment/detail/DetailFragment.kt
@@ -5,18 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.lucassimao.androidteststudy.R
+import androidx.fragment.app.viewModels
+import com.lucassimao.androidteststudy.databinding.FragmentDetailBinding
+import com.lucassimao.androidteststudy.presentation.fragment.PlaylistViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 
 @AndroidEntryPoint
 class DetailFragment : Fragment() {
+    private lateinit var binding: FragmentDetailBinding
+    private val viewModel by viewModels<PlaylistViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ): View {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+
+        setupLoader()
+        setupUI()
+
+        return binding.root
+    }
+
+    private fun setupLoader() {
+        viewModel.loader.observe(viewLifecycleOwner) { loading ->
+            when (loading) {
+                true -> binding.pbDetailLoader.visibility = View.VISIBLE
+                else -> binding.pbDetailLoader.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+    private fun setupUI() {
+        viewModel.getPlaylistDetail.observe(viewLifecycleOwner) {
+            binding.apply {
+                detailName.text = it.getOrNull()!!.name
+                playlistDetail.text = it.getOrNull()!!.details
+            }
+        }
     }
 
 }
